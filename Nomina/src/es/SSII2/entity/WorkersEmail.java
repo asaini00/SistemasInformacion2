8a83768b69cf3fa93514e8c6c1e70494b8b456ba
Alvaro <Alvaro@Alvaro-PC>
2016-04-06 14:28:47 +0200

    creacion de las clases necesarias

---
+++ Nomina/src/es/SSII2/entity/WorkersEmail.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.SSII2.entity;
+
+import java.util.ArrayList;
+import es.SSII2.entity.WorkersID;
+/**
+ *
+ * @author Alvaro
+ */
+public class WorkersEmail {
+    ArrayList<WorkersID> arrayWorkers;
+
+    public WorkersEmail(ArrayList<WorkersID> trabajador) {
+        this.arrayWorkers = trabajador;
+    }
+    
+    
+    
+}
